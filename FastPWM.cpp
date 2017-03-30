3bb3d2dc1d159a3c575577d1f066ec3c126fc177
willBoyd8 <willBoyd8@users.noreply.github.com>
2017-03-29 19:58:59 -0500

    Added the docs comment

--- FastPWM.cpp
+++ FastPWM.cpp
@@ -1,9 +1,25 @@
-/*
- * FastPWM.cpp
- *
- *  Created on: Feb 25, 2017
- *      Author: Will
- */
+// FastPWM.cpp
+//
+// Copyright 2017 UAH Electric Vehicle Club
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// 	http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/* -------------------------------------------------------------- */
+// The FastPWM Library, forked from Drew's original motor 
+// controller by Will Boyd. The original code was partially based 
+// off a demo from 
+// "www.righto.com/2009/07/secrets-of-arduino-pwm.html"
+/* -------------------------------------------------------------- */
 
 #include "FastPWM.h"
 
